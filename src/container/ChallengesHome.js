5257b792031d6ae962feb3b2a137329ee1b6ccce
Shalini M <mshalini04@gmail.com>
2018-06-03 18:54:50 -0700

    Add Label/challanges/Icon to challange page

--- src/container/ChallengesHome.js
+++ src/container/ChallengesHome.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react';
-
+import { Header, Label, Icon, List, Container } from 'semantic-ui-react';
 // import ChallengesList from '../components/ChallengesHome/ChallengesList';
 
 class ChallengesHomePage extends Component {
@@ -12,10 +12,20 @@ class ChallengesHomePage extends Component {
   }
   render() {
     return (
-      <Fragment>
-        <h1>Challenge Home Page</h1>
-        <p>this is the paragraph</p>
-      </Fragment>
+      <Container text>
+        <Header> Challenges </Header> 
+        <List>
+          <List.Item>
+          <Label as="a" color="red" ribbon>
+              Level 1
+          </Label>
+          <List.Content>
+            <span> challange 1 </span>
+          </List.Content>
+          <Icon name="trophy" corner/>
+          </List.Item>
+        </List>   
+      </Container>
     );
   }
 }
